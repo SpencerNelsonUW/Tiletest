f391911e2be084e64d0bb0ed9d4745b11d870279
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-12-19 12:41:19 -0800

    imports

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import Display from "./components/Display/Display";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Tile from "./components/Tile/Tile";
+import Display from "../components/Display/Display";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Tile from "../components/Tile/Tile";
 import Homestead from "./components/Tile/Homestead";
 import LoggingCamp from "./components/Tile/LoggingCamp";
 import Tannery from "./components/Tile/Tannery";
 import React, { useState } from "react";
-import Login from "./components/login/login.js";
+import Login from "../components/login/login.js";
 
 
 import {
