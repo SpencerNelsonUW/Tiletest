60c72cb52f1ca9b7e25f07f400db98e0ae4c5d92
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-12-19 12:28:26 -0800

    remove logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Display from "./components/Display/Display";
 import Header from "./components/Header/Header";
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Display from "./components/Display/Display";
 import Header from "./components/Header/Header";
