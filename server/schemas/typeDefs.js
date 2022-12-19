c76079561549efc281228b555b513f6c656fda5c
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-12-19 12:30:31 -0800

    t

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,12 +3,23 @@ const gql = require('graphql-tag');
 const typeDefs = gql`
     type User{
         id:ID!
+        email:String!
         username:String!
         password:String!
     }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query{
-            getUsers:[User]
-        }
+        getUsers:[User]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
 `;
 
 module.exports = typeDefs;
